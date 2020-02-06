dfa140b038f4b0a422489f532ebe36a8ae53e1b7
Tordai Janos <janos.tordai@zamg.ac.at>
2020-02-06 15:27:03 +0000

    defore orm

--- myproject/settings.py
+++ myproject/settings.py
@@ -129,4 +129,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_URL = 'login'
