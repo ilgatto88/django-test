504223824f2dba42bd233dcd867a1372988b61b7
Tordai Janos <janos.tordai@zamg.ac.at>
2020-02-07 12:08:30 +0000

    before deploy

--- boards/views.py
+++ boards/views.py
@@ -8,6 +8,7 @@ from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.urls import reverse
 
 def home(request):
     boards = Board.objects.all()
@@ -59,15 +60,26 @@ def new_topic(request, pk):
 
 @login_required
 def reply_topic(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk = pk, pk = topic_pk)
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit = False)
+            post = form.save(commit=False)
             post.topic = topic
             post.created_by = request.user
             post.save()
-            return redirect('topic_posts', pk = pk, topic_pk = topic_pk)
+
+            topic.last_updated = timezone.now()
+            topic.save()
+
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
@@ -118,11 +130,16 @@ class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'topic_posts.html'
-    paginate_by = 2
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
+            
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
